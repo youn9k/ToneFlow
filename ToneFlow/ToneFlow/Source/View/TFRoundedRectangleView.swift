0b30f5a5b180af180c6cbfcb5fe432e076a4f75c
youn9k <ace_lephant@naver.com>
2025-04-04 01:04:19 +0900

    FEAT :: 이펙터, 앰프 백그라운드 뷰 분리

---
+++ ToneFlow/ToneFlow/Source/View/TFRoundedRectangleView.swift
@@ -0,0 +1,25 @@
+//
+//  TFRoundedRectangleView.swift
+//  ToneFlow
+//
+//  Created by YoungK on 4/4/25.
+//
+
+import SwiftUI
+
+struct TFRoundedRectangleView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(.sub4)
+            .strokeBorder(Color.white, lineWidth: 1)
+            .tfShadow(alpha: 0.25, blur: 20)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.background.ignoresSafeArea()
+        TFRoundedRectangleView()
+            .frame(width: 150, height: 200)
+    }
+}
