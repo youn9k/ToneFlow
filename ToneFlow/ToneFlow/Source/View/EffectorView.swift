e3c5f6d17ba65767133fe32370778cf62d8469da
youn9k <ace_lephant@naver.com>
2025-04-05 21:31:00 +0900

    FEAT :: 이펙터 뷰 중복 코드 제거

--- ToneFlow/ToneFlow/Source/View/EffectorView.swift
+++ ToneFlow/ToneFlow/Source/View/EffectorView.swift
@@ -29,30 +29,16 @@ struct EffectorView: View {
                 TFRoundedRectangleView()
                 
                 VStack(spacing: 0) {
-                    KnobView(knobValue: $knobValue)
-                        .knobFill(.POINT)
-                        .frame(width: knobSize, height: knobSize)
-                        .tfShadow(alpha: 0.20, blur: 10)
-                    
-                    HStack {
-                        Text("Min")
-                        Spacer()
-                        Text("Max")
-                    }
-                    .tfFont(.t6(.medium))
-                    .foregroundStyle(.gray800)
-                    .frame(width: knobSize)
-                    
-                    Text(title)
-                        .tfFont(.t4(.medium))
-                        .foregroundColor(.gray800)
-                        .padding(.top, 12)
-                        .padding(.bottom, 10)
+                    TFKnobGroupView(
+                        title: title,
+                        knobValue: $knobValue,
+                        knobSize: knobSize
+                    )
                     
                     Circle()
                         .foregroundStyle(isOn ? .POINT_2 : .gray400)
                         .frame(width: 5, height: 5)
-                        .padding(.bottom, 10)
+                        .padding(.bottom, 14)
                     
                     Button {
                         isOn.toggle()
@@ -80,19 +66,35 @@ struct EffectorView: View {
         Color.background.ignoresSafeArea()
         ScrollView {
             VStack {
-                EffectorView(title: "Compressor", isOn: .constant(true), knobValue: .constant(0.5))
-                    .frame(width: 100, height: 220)
-
-                EffectorView(title: "Compressor", isOn: .constant(true), knobValue: .constant(0.5))
-                    .frame(width: 150, height: 220)
-
-                EffectorView(title: "Compressor", isOn: .constant(true), knobValue: .constant(0.5))
-                    .frame(width: 171, height: 257)
-                    .padding(.bottom, 70)
+                EffectorView(
+                    title: "Compressor",
+                    isOn: .constant(true),
+                    knobValue: .constant(0.5)
+                )
+                .frame(width: 100, height: 220)
                 
-                EffectorView(title: "Compressor", isOn: .constant(true), knobValue: .constant(0.5))
-                    .frame(width: 300, height: 220)
-                    .padding(.bottom, 70)
+                EffectorView(
+                    title: "Compressor",
+                    isOn: .constant(true),
+                    knobValue: .constant(0.5)
+                )
+                .frame(width: 150, height: 220)
+                
+                EffectorView(
+                    title: "Compressor",
+                    isOn: .constant(true),
+                    knobValue: .constant(0.5)
+                )
+                .frame(width: 171, height: 257)
+                .padding(.bottom, 70)
+                
+                EffectorView(
+                    title: "Compressor",
+                    isOn: .constant(true),
+                    knobValue: .constant(0.5)
+                )
+                .frame(width: 300, height: 220)
+                .padding(.bottom, 70)
             }
         }
         .scrollIndicators(.never)
