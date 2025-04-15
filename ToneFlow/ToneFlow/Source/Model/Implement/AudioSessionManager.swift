5d004c73cd6e63b075b705c61b2cd0578fa21fbc
youn9k <ace_lephant@naver.com>
2025-04-16 00:57:23 +0900

    REFACTOR :: AudioSessionManager Publisher 리팩토링
    Updates `AudioSessionManager` to use a shared `routeChangePublisher` for `availableInputsPublisher`, `currentInputPublisher`, and `currentOutputPublisher`.
    
    This ensures that these publishers emit updates whenever the audio session route changes, providing more reactive updates.

--- ToneFlow/ToneFlow/Source/Model/Implement/AudioSessionManager.swift
+++ ToneFlow/ToneFlow/Source/Model/Implement/AudioSessionManager.swift
@@ -1,10 +1,3 @@
-//
-//  AudioSessionManager.swift
-//  ToneFlow
-//
-//  Created by YoungK on 4/15/25.
-//
-
 import AVFAudio
 import Combine
 
@@ -12,28 +5,38 @@ final class AudioSessionManager: AudioSessionManageable {
     private var cancellables = Set<AnyCancellable>()
     private let session = AVAudioSession.sharedInstance()
     
-    let availableInputsPublisher: AnyPublisher<[AudioPortDescription], Never>
-    let currentInputPublisher: AnyPublisher<AudioPortDescription?, Never>
-    let currentOutputPublisher: AnyPublisher<AudioPortDescription?, Never>
+    var availableInputsPublisher: AnyPublisher<[AudioPortDescription], Never> {
+        routeChangePublisher
+            .map { _ in AVAudioSession.sharedInstance().availableInputs ?? [] }
+            .eraseToAnyPublisher()
+    }
+    
+    var currentInputPublisher: AnyPublisher<AudioPortDescription?, Never> {
+        routeChangePublisher
+            .map { _ in AVAudioSession.sharedInstance().currentRoute.inputs.first }
+            .eraseToAnyPublisher()
+    }
+    
+    var currentOutputPublisher: AnyPublisher<AudioPortDescription?, Never> {
+        routeChangePublisher
+            .map { _ in AVAudioSession.sharedInstance().currentRoute.outputs.first }
+            .eraseToAnyPublisher()
+    }
     
     init() {
-        availableInputsPublisher = session.publisher(for: \.availableInputs)
-            .map { $0 ?? [] }
-            .eraseToAnyPublisher()
-        
-        currentInputPublisher = session.publisher(for: \.currentRoute)
-            .map(\.inputs.first)
-            .eraseToAnyPublisher()
-        
-        currentOutputPublisher = session.publisher(for: \.currentRoute)
-            .map(\.outputs.first)
-            .eraseToAnyPublisher()
-        
         setupAudioSession()
     }
     
+    private let routeChangePublisher = NotificationCenter.default
+        .publisher(for: AVAudioSession.routeChangeNotification)
+        .share() // 퍼블리셔 공유
+
     private func setupAudioSession() {
-        try? session.setCategory(.playAndRecord, mode: .default, options: [.mixWithOthers, .allowBluetooth, .allowBluetoothA2DP])
+        try? session.setCategory(.playAndRecord, mode: .default, options: [
+            .mixWithOthers,
+            .allowBluetooth,
+            .allowBluetoothA2DP
+        ])
         try? session.setActive(true)
         try? session.setInputGain(0.5)
     }
